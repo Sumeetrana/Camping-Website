7b15b218c6b51be18576883ea7f7882a65103d6e
Sumeetrana <sumeetsinh28@gmail.com>
2020-02-22 03:23:52 +0530

    Final touches

--- routes/auth.js
+++ routes/auth.js
@@ -33,7 +33,8 @@ router.get("/login", (req, res) => {
 
 router.post("/login", passport.authenticate("local",{
     successRedirect: '/camps',
-    failureRedirect: '/login'
+    failureRedirect: '/login',
+    failureFlash: true
 }), (req, res) => {
 })
 
